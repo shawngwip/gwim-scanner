5c3bde2c53757a017b0c92c51b9dc8264c050fe7
dyuangwim <dyuangwim@gmail.com>
2025-07-07 16:47:49 +0800

    Update test_display.py

--- test_display.py
+++ test_display.py
@@ -1,21 +1,29 @@
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 import time
 
+# 设置面板参数（你4片横拼，32行 × 64列 × 4 = 256x32）
 options = RGBMatrixOptions()
 options.rows = 32
 options.cols = 64
-options.chain_length = 2
+options.chain_length = 4
 options.parallel = 1
 options.hardware_mapping = 'regular'
 
 matrix = RGBMatrix(options=options)
+canvas = matrix.CreateFrameCanvas()
+
+# 用内建字体，不加载 .bdf
+font = graphics.Font()
+font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/6x13.bdf")
+color = graphics.Color(255, 255, 0)
+
+pos = 10
 
 while True:
-    matrix.Fill(255, 0, 0)  # 全红
-    time.sleep(1)
-    matrix.Clear()
-    matrix.Fill(0, 255, 0)  # 全绿
-    time.sleep(1)
-    matrix.Clear()
-    matrix.Fill(0, 0, 255)  # 全蓝
-    time.sleep(1)
+    canvas.Clear()
+    graphics.DrawText(canvas, font, pos, 20, color, "GWIM READY")
+    pos -= 1
+    if pos < -100:
+        pos = 64
+    time.sleep(0.05)
+    canvas = matrix.SwapOnVSync(canvas)
