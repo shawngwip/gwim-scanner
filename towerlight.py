8c11354cb72d721c76b3f59edde99c3e6d2ca6f0
dyuangwim <dyuangwim@gmail.com>
2025-07-09 12:16:45 +0800

    Update towerlight.py

--- towerlight.py
+++ towerlight.py
@@ -20,3 +20,14 @@ time.sleep(5)
 GPIO.output(RED_PIN, GPIO.HIGH)
 print("测试完毕")
 GPIO.cleanup()
+
+
+import RPi.GPIO as GPIO
+import time
+
+PIN = 5
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN, GPIO.OUT)
+
+GPIO.output(PIN, GPIO.HIGH)  # 默认状态：灯灭（NO） / 亮（NC）
+time.sleep(999)              # 保持状态让你测试
