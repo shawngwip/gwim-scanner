4e2242e09017cc811e4c11a7bee41bfc1f1d0e9a
dyuangwim <dyuangwim@gmail.com>
2025-07-10 08:14:38 +0800

    Delete font_test.py

--- font_test.py
+++
@@ -1,8 +0,0 @@
-from rgbmatrix import graphics
-
-try:
-    font = graphics.Font()
-    font.LoadFont("/home/pi/gwim-scanner/fonts/helvB12-vp.bdf")
-    print("✅ Font loaded successfully")
-except Exception as e:
-    print(f"❌ Font Load Error: {e}")
